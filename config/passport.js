801bf75f7a42da3b85458456eb9ccb1d68551f95
Tolulope Odueke <42325628+tolulope-od@users.noreply.github.com>
2019-07-23 14:23:37 +0100

    #167453183 Refactor codebase (#3)
    * chore(maitainance): refactor codebase to ES6 JS
    
    Refactors codebase to use ES6+ syntax
    Adds babel to transpile ES6 code
    Adds run server script
    
    [Finishes #167453183]

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,28 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
+import passport from "passport";
+import { Strategy } from "passport-local";
+import mongoose from "mongoose";
+
+const LocalStrategy = Strategy;
 const User = mongoose.model("User");
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: "user[email]",
-            passwordField: "user[password]"
-        },
-        function(email, password, done) {
-            User.findOne({ email: email })
-                .then(function(user) {
-                    if (!user || !user.validPassword(password)) {
-                        return done(null, false, {
-                            errors: { "email or password": "is invalid" }
-                        });
-                    }
+  new LocalStrategy(
+    {
+      usernameField: "user[email]",
+      passwordField: "user[password]"
+    },
+    (email, password, done) => {
+      User.findOne({ email: email })
+        .then(user => {
+          if (!user || !user.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
 
-                    return done(null, user);
-                })
-                .catch(done);
-        }
-    )
+          return done(null, user);
+        })
+        .catch(done);
+    }
+  )
 );
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const fs = require("fs"),
-    http = require("http"),
-    path = require("path"),
-    methods = require("methods"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    session = require("express-session"),
-    cors = require("cors"),
-    passport = require("passport"),
-    errorhandler = require("errorhandler"),
-    mongoose = require("mongoose");
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+import errorhandler from "errorhandler";
+import mongoose from "mongoose";
+import logger from "morgan";
+import methodOverride from "method-override";
+
+import routes from "./routes";
+import User from "./models/User";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -18,42 +17,40 @@ const app = express();
 app.use(cors());
 
 // Normal express config defaults
-app.use(require("morgan")("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(require("method-override")());
+app.use(methodOverride());
 app.use(express.static(__dirname + "/public"));
 
 app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
+  session({
+    secret: "authorshaven",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
 );
 
 if (!isProduction) {
-    app.use(errorhandler());
+  app.use(errorhandler());
 }
 
 if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI);
 } else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
+  mongoose.connect("mongodb://localhost/conduit");
+  mongoose.set("debug", true);
 }
 
-require("./models/User");
-
-app.use(require("./routes"));
+app.use(routes);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -61,33 +58,33 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-    app.use(function(err, req, res, next) {
-        console.log(err.stack);
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
 
-        res.status(err.status || 500);
+    res.status(err.status || 500);
 
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
-    });
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
 });
 
 // finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on port " + server.address().port);
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + server.address().port);
 });
