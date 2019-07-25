257bb440cd6dc3e6cd2dd12b9e293265980fa993
Tolulope Odueke <tolulope.od@gmail.com>
2019-07-25 15:34:31 +0100

    feature(auth): user can sign out
    Create functionality for user to sign out
    Create authentication middleware
    Create server response module
    Add pretest script
    Change blacklist table model column
    Add database service to travis CI
    
    [Starts #167165057]

---
+++ controllers/AuthController.js
@@ -0,0 +1,89 @@
+import models from '../database/models';
+import Helper from '../helpers/Auth';
+import ServerResponse from '../modules';
+
+const { successResponse } = ServerResponse;
+const { BlacklistedToken, User } = models;
+
+/**
+ *
+ *
+ * @export
+ * @class AuthController
+ */
+export default class AuthController {
+  /**
+   *
+   * @static
+   * @param {object} req express request object
+   * @param {object} res express response object
+   * @returns {object} returns user data
+   * @memberof AuthController
+   */
+  static async signUp(req, res) {
+    const {
+      firstName, lastName, email, password
+    } = req.body;
+
+    const foundUser = await User.findOne({ where: { email } });
+    if (foundUser) {
+      return res.status(409).send({
+        message: 'This User already exist'
+      });
+    }
+    const hashedPassword = Helper.hashPassword(password);
+    const verificationToken = Helper.hashUserData(email);
+    const user = {
+      firstName,
+      lastName,
+      email,
+      isVerified: false,
+      verificationToken,
+      password: hashedPassword,
+      type: 'user'
+    };
+
+    const registeredUser = await User.create(user);
+    const token = Helper.createToken({
+      id: registeredUser.id,
+      email
+    });
+    return res.status(201).send({
+      message: 'Your Account has been created successfully!',
+      user: {
+        token,
+        id: registeredUser.id,
+        firstName: registeredUser.firstName,
+        lastName: registeredUser.lastName,
+        email: registeredUser.email,
+        isVerified: registeredUser.isVerified,
+        verificationToken
+      }
+    });
+  }
+
+  /**
+   *Method to log a user out
+   *
+   * @static
+   *
+   * @param {object} req
+   * @param {object} res
+   * @param {function} next
+   *
+   * @returns {object} response object with message
+   *
+   * @memberof AuthController
+   */
+  static async logout(req, res, next) {
+    try {
+      const { authorization } = req.headers;
+      const { id } = req.user;
+      const token = authorization.split(' ')[1];
+      await BlacklistedToken.create({ token, userId: id });
+      return successResponse(res, 200, { message: 'Successfully logged out' });
+    } catch (err) {
+      return next(err);
+    }
+  }
+}
---
+++ middlewares/Authentication.js
@@ -0,0 +1,66 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+import ServerResponse from '../modules';
+import models from '../database/models';
+
+const { config } = dotenv;
+const { errorResponse } = ServerResponse;
+const { SECRET_KEY } = process.env;
+const { BlacklistedToken } = models;
+
+config();
+
+/**
+ *
+ *
+ * @export
+ * @class Authentication
+ */
+export default class Authentication {
+  /**
+   *Verify token for Authenticated routes
+   *
+   * @static
+   *
+   * @param {object} req
+   * @param {object} res
+   * @param {function} next
+   *
+   * @returns {function} next function
+   *
+   * @memberof Authentication
+   */
+  static async verifyToken(req, res, next) {
+    try {
+      const bearer = req.headers.authorization;
+      if (!bearer) {
+        return errorResponse(res, 401, { message: 'No token provided' });
+      }
+
+      const token = bearer.split(' ')[1];
+      const blacklistedToken = await BlacklistedToken.findOne({
+        where: { token }
+      });
+
+      if (blacklistedToken) {
+        return errorResponse(res, 403, {
+          message: 'Invalid token provided, please sign in'
+        });
+      }
+
+      jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+          return errorResponse(res, 403, {
+            message: 'Invalid token provided'
+          });
+        }
+
+        req.user = decoded;
+        return next();
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }
+}
