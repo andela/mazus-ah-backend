2c2999b859b240503a2b5806946dc709ced3f8e9
VictorMaria Ajayi <victor.abayomi@outlook.com>
2019-07-26 09:26:39 +0100

    feature(verification): send verification email [Starts #167165055]

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import AuthController from '../controllers/AuthController';
 
-const { signUp } = AuthController;
+const { signUp, verifyEmail } = AuthController;
 
 const router = Router();
 
 router.post('/signup', signUp);
+router.patch('/:email/:token', verifyEmail);
 
 export default router;
