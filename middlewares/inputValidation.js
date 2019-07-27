32a424850501a97710d41245cccd512f050699c3
Odunayo Okebunmi <okebunmiodunayo@gmail.com>
2019-07-27 08:27:20 +0100

    feat(signup-validation): add validation middleware to route (#15)
    use express-validator to validate users signup input
    document feature in swagger
    [Finishes #167165056]

---
+++ middlewares/inputValidation.js
@@ -0,0 +1,50 @@
+import { check, validationResult } from 'express-validator/check';
+
+const validate = {
+  signup: [
+    check('email')
+      .not()
+      .isEmpty({ ignore_whitespace: true })
+      .withMessage('Email is required')
+      .isEmail()
+      .trim()
+      .withMessage('Please input a valid email address'),
+    check('firstName')
+      .not()
+      .isEmpty({ ignore_whitespace: true })
+      .withMessage('First name is required')
+      .isAlpha()
+      .trim()
+      .withMessage('First name can only contain letters'),
+    check('lastName')
+      .not()
+      .isEmpty({ ignore_whitespace: true })
+      .withMessage('Last name is required')
+      .isAlpha()
+      .trim()
+      .withMessage('Last name can only contain letters'),
+    check('password')
+      .not()
+      .isEmpty({ ignore_whitespace: true })
+      .withMessage('Password is required')
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]/, 'i')
+      .withMessage('Password must contain at least one uppercase letter, one lowercase letter and one numeric digit')
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage('Password must be at least 8 characters'),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      const errorMessage = {};
+      if (!errors.isEmpty()) {
+        errors.array({ onlyFirstError: true }).forEach((error) => {
+          errorMessage[error.param] = error.msg;
+        });
+        return res.status(400).json({
+          errors: errorMessage,
+        });
+      }
+      return next();
+    }
+  ]
+};
+export default validate;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import AuthController from '../controllers/AuthController';
 import AuthMiddleware from '../middlewares/Authentication';
+import Validate from '../middlewares/inputValidation';
 
 const { signUp, logout } = AuthController;
 const { verifyToken } = AuthMiddleware;
 
 const router = Router();
 
-router.post('/signup', signUp);
+router.post('/signup', Validate.signup, signUp);
 router.post('/logout', verifyToken, logout);
+
 
 export default router;
