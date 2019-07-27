af980a7c897c4c4f19583c42ef889643bd5efaa6
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-07-27 22:03:41 +0100

    fix(hound): fix hound error
    fix my eslint
    properly fix hound errors

--- __tests__/profileRoute.test.js
+++ __tests__/profileRoute.test.js
@@ -1,4 +1,3 @@
-import sinon from 'sinon';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
@@ -17,11 +16,11 @@ describe('Profile test', () => {
       firstName: 'Luis',
       lastName: 'Gucci',
       email: 'luicGucci@outfit.com',
-      password: 'passWORD122'
+      password: 'passWORD122',
     };
     chai
       .request(app)
-      .post(`/api/v1/auth/signup`)
+      .post('/api/v1/auth/signup')
       .send(user)
       .end((err, res) => {
         const { token } = res.body.user;
@@ -30,7 +29,6 @@ describe('Profile test', () => {
       });
   });
   describe('Create profile', () => {
-    
     it('should not create a profile if unsupported input format is provided', (done) => {
       chai
         .request(app)
@@ -43,9 +41,9 @@ describe('Profile test', () => {
           expect(res.body.errors[0]).to.eql('Avatar is not a valid URL, please input a valid URL');
           expect(res.body.errors[1]).to.eql('Bio is not a valid string, please input a valid string');
           done();
-        })
-    })
-   
+        });
+    });
+
     it('should create a profile successfully when all field are inputed correctly', (done) => {
       chai
         .request(app)
@@ -62,9 +60,7 @@ describe('Profile test', () => {
           expect(res.body.profile.avatar).to.eql(mockProfile.correctProfile.avatar);
           expect(res.body.profile.bio).to.eql(mockProfile.correctProfile.bio);
           done();
-             
-        })
-
+        });
     });
 
     it('should not create a profile if profile has already been created', (done) => {
@@ -75,16 +71,14 @@ describe('Profile test', () => {
         .send(mockProfile.correctProfile)
         .end((err, res) => {
           expect(res.status).to.equal(409);
-          expect(res.body).to.have.property('errors')
+          expect(res.body).to.have.property('errors');
           expect(res.body).to.have.nested.property('errors');
           done();
-        })
+        });
     });
-
   });
 
   describe('Edit profile', () => {
-
     it('should throw error if input is unsupported', (done) => {
       chai
         .request(app)
@@ -99,7 +93,7 @@ describe('Profile test', () => {
           expect(res.body.errors[2]).to.eql('FirstName is not a valid String, please input a valid string');
           expect(res.body.errors[3]).to.eql('LastName is not a valid string, please input a valid string');
           done();
-        })
+        });
     });
 
     it('should successfully update the profile if all input are correct', (done) => {
@@ -121,7 +115,7 @@ describe('Profile test', () => {
           expect(res.body.profile.firstName).to.eql(mockProfile.correctProfilEdit.firstName);
           expect(res.body.profile.lastName).to.eql(mockProfile.correctProfilEdit.lastName);
           done();
-        })
+        });
     });
   });
 
@@ -131,7 +125,7 @@ describe('Profile test', () => {
         .request(app)
         .get(`${url}/3000`)
         .set('Authorization', `Bearer ${validToken}`)
-        .end((err,res) => {
+        .end((err, res) => {
           expect(res.status).to.eql(200);
           expect(res.body).to.have.property('errors');
           expect(res.body.errors[0]).to.eql('No User with the specified ID was found');
@@ -153,10 +147,10 @@ describe('Profile test', () => {
           expect(res.body.profile).to.have.property('firstName');
           expect(res.body.profile).to.have.property('lastName');
           expect(res.body.profile).to.have.property('verificationToken');
-          expect(res.body.profile).to.have.property('isVerified');        
+          expect(res.body.profile).to.have.property('isVerified');
           expect(res.body.profile).to.have.property('type');
           done();
-        })
+        });
     });
   });
 });
--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,7 @@ import swagger from './swagger.json';
 import logout from './auth/logout.json';
 import signup from './auth/signup.json';
 import createProfile from './profile/createAndEditProfile.json';
-import viewProfile from './profile/viewProfile.json'
+import viewProfile from './profile/viewProfile.json';
 
 swagger.paths['/auth/signup'] = signup;
 swagger.paths['/auth/logout'] = logout;
