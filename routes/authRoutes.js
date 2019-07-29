87528bdf3bd0b3e02e0aef6e79527224368ecf68
Emeka Ofe <40548599+mekzy-o@users.noreply.github.com>
2019-07-29 10:10:44 +0100

    feat(signing): add user login endpoint (#21)
    Added endpoint (with tests) to enable user login to their accounts
    Documented login endpoint using swagger.json
    
    [Finishes #167165054]

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,13 @@ import AuthController from '../controllers/AuthController';
 import AuthMiddleware from '../middlewares/Authentication';
 import Validate from '../middlewares/inputValidation';
 
-const { signUp, logout } = AuthController;
+const { signUp, userSignin, logout } = AuthController;
 const { verifyToken } = AuthMiddleware;
 
 const router = Router();
 
 router.post('/signup', Validate.signup, signUp);
+router.post('/signin', userSignin);
 router.post('/logout', verifyToken, logout);
 
 
