a86b467b73bfca53bfbdc395b04c8575af8383dd
Samuel Ocran <sammiestt@gmail.com>
2019-07-29 17:27:55 +0100

    feat(social-login): Enable social login (#20)
    Add social media authentication routes to authRoute.js
    Create a passportStretegy.js file with required passport configuration
    Add passport authentication to all social routes to handle authentication errors
    Add socialLogin method to AuthController.js
    FindOrCreate new user account
    
    [Finishes ##167165059]

---
+++ helpers/passportStrategies.js
@@ -0,0 +1,30 @@
+import passport from 'passport';
+import facebook from 'passport-facebook';
+import google from 'passport-google-oauth';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const fbConfig = {
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+  profileFields: ['id', 'displayName', 'email'],
+};
+
+const googleConfig = {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  profileFields: ['id', 'displayName', 'email', 'photos'],
+};
+
+passport.use(new facebook.Strategy(
+  fbConfig,
+  (accessToken, refreshToken, profile, done) => done(null, profile)
+));
+
+passport.use(new google.OAuth2Strategy(
+  googleConfig,
+  (accessToken, refreshToken, profile, done) => done(null, profile)
+));
