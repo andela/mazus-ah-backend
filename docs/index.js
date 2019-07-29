931c4983a593c862c129cf5f81f61a596a9c490d
Samuel Ocran <sammiestt@gmail.com>
2019-07-29 17:27:55 +0100

    feat(social-login): Enable social login (#20)
    Add social media authentication routes to authRoute.js
    Create a passportStretegy.js file with required passport configuration
    Add passport authentication to all social routes to handle authentication errors
    Add socialLogin method to AuthController.js
    FindOrCreate new user account
    
    [Finishes ##167165059]

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,14 @@ import swagger from './swagger.json';
 import logout from './auth/logout.json';
 import signup from './auth/signup.json';
 import login from './auth/login.json';
+import facebookLogin from './auth/facebook.json';
+import googleLogin from './auth/google';
 
 swagger.paths['/auth/signup'] = signup;
 swagger.paths['/auth/signin'] = login;
 swagger.paths['/auth/logout'] = logout;
+swagger.paths['/auth/facebook'] = facebookLogin;
+swagger.paths['/auth/google'] = googleLogin;
 
 
 export default swagger;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import passport from 'passport';
 import AuthController from '../controllers/AuthController';
 import AuthMiddleware from '../middlewares/Authentication';
 import Validate from '../middlewares/inputValidation';
+import '../helpers/passportStrategies';
 
-const { signUp, userSignin, logout } = AuthController;
+const {
+  signUp,
+  userSignin,
+  logout,
+  socialLogin,
+} = AuthController;
+
 const { verifyToken } = AuthMiddleware;
 
 const router = Router();
@@ -12,5 +20,10 @@ router.post('/signup', Validate.signup, signUp);
 router.post('/signin', userSignin);
 router.post('/logout', verifyToken, logout);
 
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+router.get('/facebook/callback', passport.authenticate('facebook', { session: false }), socialLogin);
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { session: false }), socialLogin);
 
 export default router;
