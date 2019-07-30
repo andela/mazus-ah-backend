c0567f50d7b47b774121e1421ebfeeb31861ead6
VictorMaria <41482184+VictorMaria@users.noreply.github.com>
2019-07-30 15:41:41 +0100

    #167165055 Send user verification email (#28)
    * feature(verification): send verification email to user
    
    implement feature to send verification email to users via SendGrid
    verifies a user via the link sent in the email
    
    [Finishes #167165055]

--- __tests__/authRoutes.test.js
+++ __tests__/authRoutes.test.js
@@ -14,6 +14,7 @@ const { BlacklistedToken } = models;
 let validUserToken;
 const blacklistedToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3ROYW1lIjoiSm9obiIsImlhdCI6MTU2NDAwOTA5NCwiZXhwIjoxNTY0MDEyNjk0fQ.J5ktoXlmLxOtV8R16sNPMXXeydwRdCA8h6Cep-AzZnc';
 
+
 describe('User signup tests', () => {
   describe('test for user signup', () => {
     it('should register a user successfully when all fields are inputed correctly', (done) => {
