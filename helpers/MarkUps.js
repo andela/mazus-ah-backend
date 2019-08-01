810f69465867e73f5c1e85f49441cfcb68df2ba1
VictorMaria Ajayi <victor.abayomi@outlook.com>
2019-08-01 17:25:42 +0100

    correct markup tags

--- helpers/MarkUps.js
+++ helpers/MarkUps.js
@@ -1,7 +1,7 @@
 const MarkUps = {
   verified: `
     <center>
-    <img src = 'https://res.cloudinary.com/dsqyhgfws/image/upload/v1564669965/authors_haven_e1havh.png style='height:270px;width:270px';'>
+    <img src = 'https://res.cloudinary.com/dsqyhgfws/image/upload/v1564669965/authors_haven_e1havh.png' style='height:270px; width:270px';'>
     <h1 style='font-family:helvetica;'>Your email has been verified</h1>
     </center>
     `,
@@ -13,7 +13,7 @@ const MarkUps = {
     `,
   incorrectCredentials: `
     <center>
-    <img src = 'https://res.cloudinary.com/dsqyhgfws/image/upload/v1564669965/authors_haven_e1havh.png style='height:270px; width:270px';'>
+    <img src = 'https://res.cloudinary.com/dsqyhgfws/image/upload/v1564669965/authors_haven_e1havh.png' style='height:270px; width:270px';'>
     <h1 style='font-family:helvetica;'>Your credentials are incorrect</h1>
     </center>
     `,
