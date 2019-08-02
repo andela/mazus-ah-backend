542eb9fb77c3537473aef1ce0ca701f1085cfbc2
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-08-02 14:35:23 +0100

    ft(database): generate migration, models and seeders file

---
+++ database/models/notification.js
@@ -0,0 +1,32 @@
+/* eslint-disable func-names */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+module.exports = (sequelize, DataTypes) => {
+  const Notification = sequelize.define('Notification', {
+    payload: {
+      allowNull: false,
+      type: DataTypes.JSON,
+    },
+    read: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+    },
+    receiverId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    type: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    }
+  }, {});
+  Notification.associate = function(models) {
+    // associations can be defined here
+    Notification.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    })
+  };
+  return Notification;
+};
