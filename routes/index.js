4e39db4ec59fcb2e0167933ba95caa706c109844
Tolulope Odueke <42325628+tolulope-od@users.noreply.github.com>
2019-08-07 16:55:53 +0100

    feature(comment):like a comment (#43)
    Add functionality to like a single comment
    Change comment model to include likes
    Change like model to include like boolean values
    
    [Finishes #167165077]

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import articleRoutes from './articleRoutes';
 import userRoutes from './userRoutes';
 import notificationRoutes from './notificationRoutes';
 import emailSubscriptionRoute from './emailSubscriptionRoute';
+import commentRoutes from './commentRoutes';
 
 const router = Router();
 
@@ -16,5 +17,6 @@ router.use('/articles', articleRoutes);
 router.use('/users', userRoutes);
 router.use('/notifications', notificationRoutes);
 router.use('/users', emailSubscriptionRoute);
+router.use('/comments', commentRoutes);
 
 export default router;
