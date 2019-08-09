36245516fa6a4cdf89874a7f49fca4b7a3794a43
Samuel Ocran <sammiestt@gmail.com>
2019-08-09 09:31:35 +0100

    feat(reports): report article (#45)
    Add feature for users to report articles
    
    [Finishes #167165076]

--- docs/index.js
+++ docs/index.js
@@ -28,6 +28,7 @@ import updateNotification from './notifications/patchNotification.json';
 import getAllBookmark from './users/getBookmark.json';
 import bookmarkArticle from './articles/bookmarkArticles.json';
 import shareArticle from './articles/shareArticles/shareByMail.json';
+import reportArticle from './articles/reportArticle.json';
 
 swagger.paths['/auth/signup'] = signup;
 swagger.paths['/auth/signin'] = login;
@@ -55,6 +56,7 @@ swagger.paths['/articles/{slug}/ratings'] = rateArticles;
 swagger.paths['/users/bookmarks'] = getAllBookmark;
 swagger.paths['/articles/{id}/bookmark'] = bookmarkArticle;
 
+swagger.paths['/articles/{slug}/report'] = reportArticle;
 swagger.paths['/notifications'] = getNotifcation;
 swagger.paths['/notifications/{id}'] = updateNotification;
 swagger.paths['/articles/trends'] = trendingArticles;
--- middlewares/commentValidations.js
+++ middlewares/commentValidations.js
@@ -23,5 +23,34 @@ const validate = {
       return next();
     }
   ],
+  validateReport: [
+    check('reportTitle')
+      .not()
+      .isEmpty({ ignore_whitespace: true })
+      .withMessage('A title is required')
+      .not()
+      .isInt()
+      .withMessage('report title is not a valid string, please input a valid string'),
+    check('reportBody')
+      .not()
+      .isEmpty({ ignore_whitespace: true })
+      .withMessage('A body is required')
+      .not()
+      .isInt()
+      .withMessage('report body is not a valid string, please input a valid string'),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      const errorMessage = {};
+      if (!errors.isEmpty()) {
+        errors.array({ onlyFirstError: true }).forEach((error) => {
+          errorMessage[error.param] = error.msg;
+        });
+        return res.status(400).json({
+          errors: errorMessage,
+        });
+      }
+      return next();
+    }
+  ],
 };
 export default validate;
