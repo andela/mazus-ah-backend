4d8660d76ae8fe0664f5d3e24036db832c87b485
Pelumi Aleshinloye <aleshinloyelukmony@gmail.com>
2019-08-09 15:17:45 +0100

    #167165071 get all users (#51)
    * feat(users): get all users
    
    add endpoint for getting all users
    document the endpoint with swagger
    
    [Finishes #167165071]

--- __tests__/getAllUsers.test.js
+++ __tests__/getAllUsers.test.js
@@ -13,8 +13,8 @@ let userId;
 describe('Testing GET users endpoint', () => {
   before((done) => {
     const user = {
-      email: 'johndoe@test.com',
-      password: 'passwordHash',
+      email: 'pelumi@test.com',
+      password: 'PasswoRD123__',
     };
     chai
       .request(app)
@@ -29,7 +29,33 @@ describe('Testing GET users endpoint', () => {
         done();
       });
   });
-
+  it('should get all Users successfully', (done) => {
+    chai
+      .request(app)
+      .get(url)
+      .set('Authorization', `Bearer ${validToken}`)
+      .end((err, res) => {
+        expect(res.status).to.eql(200);
+        expect(res.body.users).to.have.property('message');
+        expect(res.body.users.message).to.eql('Users fetched successfully');
+        expect(res.body.users).to.be.a('object');
+        expect(res.body.users).to.have.property('allUsers');
+        expect(res.body.users.allUsers[0]).to.have.property('id');
+        expect(res.body.users.allUsers[0]).to.have.property('firstName');
+        expect(res.body.users.allUsers[0]).to.have.property('lastName');
+        expect(res.body.users.allUsers[0]).to.have.property('email');
+        expect(res.body.users.allUsers[0]).to.have.property('isVerified');
+        expect(res.body.users.allUsers[0]).to.have.property('type');
+        expect(res.body.users.allUsers[0]).to.have.property('emailNotify');
+        expect(res.body.users.allUsers[0]).to.have.property('profile');
+        expect(res.body.users.allUsers[0].firstName).to.eql('Aana');
+        expect(res.body.users.allUsers[0].lastName).to.eql('Bella');
+        expect(res.body.users.allUsers[0].email).to.eql('BA@test.com');
+        expect(res.body.users.allUsers[0].isVerified).to.eql(true);
+        expect(res.body.users.allUsers[0].emailNotify).to.eql(true);
+        done();
+      });
+  });
   it('should return error 400 if id is invalid', (done) => {
     chai.request(app)
       .get(`${url}/11d509cc-b787-f-b176-fdb63cb9ed44/bookmarks`)
