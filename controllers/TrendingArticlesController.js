9feb47b78653366c4ebf0cbd305c8ef4d82fba7a
Tolulope Odueke <tolulope.od@gmail.com>
2019-08-15 14:27:30 +0100

    feat(trending-articles):get trending articles by tag
    Add optional parameter to trending articles route to get trending articles by a tag
    Refactor response format for trending articles
    Add Swagger documentation for trending articles by tag
    
    [Start #167918258]

--- controllers/TrendingArticlesController.js
+++ controllers/TrendingArticlesController.js
@@ -2,7 +2,9 @@ import models from '../database/models';
 import ServerResponse from '../modules';
 
 
-const { Article, User, Profile } = models;
+const {
+  Article, User, Profile, Sequelize: { Op },
+} = models;
 const { successResponse } = ServerResponse;
 
 /**
@@ -18,33 +20,67 @@ export default class Trending {
      *
      * @param {object} req request object
      * @param {object} res response object
+     * @param {function} next function
      *
      * @returns {object} trending data
      * @memberof Trending
      */
-  static async trend(req, res) {
-    const itsTrending = await Article.findAll({
-      limit: 10,
-      where: {
-        status: 'published'
-      },
-      include: [
-        { // Author
-          model: User,
-          as: 'author',
-          attributes: ['id', 'firstName', 'lastName', 'email'],
-          include: [{
-            model: Profile,
-            as: 'profile',
-          }]
+  static async trend(req, res, next) {
+    try {
+      const { tag } = req.query;
+      if (tag) {
+        const itsTrending = await Article.findAll({
+          limit: 10,
+          where: {
+            status: 'published',
+            tagsList: {
+              [Op.contains]: [`${tag}`],
+            },
+          },
+          include: [
+            { // Author
+              model: User,
+              as: 'author',
+              attributes: ['id', 'firstName', 'lastName', 'email'],
+              include: [{
+                model: Profile,
+                as: 'profile',
+              }]
+            },
+          ],
+          order: [
+            ['readCount', 'DESC'],
+            ['createdAt', 'DESC'],
+            ['ratings', 'DESC'],
+          ]
+        });
+        return successResponse(res, 200, 'trends', { articleCount: itsTrending.length, articles: itsTrending });
+      }
+      const itsTrending = await Article.findAll({
+        limit: 10,
+        where: {
+          status: 'published'
         },
-      ],
-      order: [
-        ['readCount', 'DESC'],
-        ['createdAt', 'DESC'],
-        ['ratings', 'DESC'],
-      ]
-    });
-    return successResponse(res, 200, 'trends', itsTrending);
+        include: [
+          { // Author
+            model: User,
+            as: 'author',
+            attributes: ['id', 'firstName', 'lastName', 'email'],
+            include: [{
+              model: Profile,
+              as: 'profile',
+            }]
+          },
+        ],
+        order: [
+          ['readCount', 'DESC'],
+          ['createdAt', 'DESC'],
+          ['ratings', 'DESC'],
+        ]
+      });
+      return successResponse(res, 200, 'trends', { articleCount: itsTrending.length, articles: itsTrending });
+    } catch (err) {
+      return next(err);
+    }
   }
 }
