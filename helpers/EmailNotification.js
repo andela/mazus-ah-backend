cd0b410e68705d6890267624d93f3826e7f2c48e
VictorMaria Ajayi <victor.abayomi@outlook.com>
2019-08-15 02:38:43 +0100

    feature(report):in-app and email notifications for reported articles add UrgentEmail js file [Starts #167893656]

--- helpers/EmailNotification.js
+++ helpers/EmailNotification.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import Postals from './Postals';
 
 /**
@@ -29,6 +30,47 @@ export default class EmailNotification {
       recipient,
       'mazus.ah@gmail.com',
       'Welcome to Authors Haven',
+      content
+    );
+  }
+
+  /**
+  *
+  * @static
+  * @param {string} recipient - recipient of the email
+  * @param {string} name - name of the recipient
+  * @param {object} payloadDetails - payload details
+  * @memberof EmailNotification
+  * @returns {function} - returns a function call
+  */
+  static sendEmailForReportedArticle(recipient, name, payloadDetails) {
+    const {
+      articleTitle,
+      articleUrl,
+      titleOfReport,
+      bodyOfReport,
+      time
+    } = payloadDetails;
+    const content = `<img src = 'https://res.cloudinary.com/dsqyhgfws/image/upload/v1565811732/Screen_Shot_2019-08-14_at_8.41.34_PM_ssj6ej.png'>
+                    <div>
+                    <h2>Hello ${name},</h2>
+                    <h2>
+                    Your article <a href = ${articleUrl}>${articleTitle}</a>
+                    was reported Today, ${moment(time).format('MMMM Do YYYY')} at ${moment(time).format('h:mm:ss a')}. 
+                    Please read through the report and promptly make changes.<br><br>
+                    REPORT DETAILS!<br>
+                    Title: ${titleOfReport}<br>
+                    Body: ${bodyOfReport}
+                    </h2>
+                    </div>
+                    <div>
+                    <h4>Read our Policies and Regulations here.</h4>
+                    </div>`;
+
+    Postals.sendEmail(
+      recipient,
+      'mazus.ah@gmail.com',
+      'Your article was reported',
       content
     );
   }
