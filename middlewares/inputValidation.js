3ac0264df205a961349896d69049f82991ec6d8b
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-08-19 16:31:10 +0100

    fix(profile): remove create profile doc

--- middlewares/inputValidation.js
+++ middlewares/inputValidation.js
@@ -124,6 +124,7 @@ const validate = {
     param('id')
       .matches((/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i))
       .withMessage('id is not valid')
+    /* istanbul ignore next-line */
       .custom(async (id) => {
         const isExist = await models.Profile.findOne({ where: { userId: id } });
         if (!isExist) {
