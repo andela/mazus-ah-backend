492bfd6245d5425e80de0f83dcd875b5908ad92f
Emeka Ofe <40548599+mekzy-o@users.noreply.github.com>
2019-09-16 18:49:36 +0100

    bg-fix(comment): add comment like array (#98)
    Returns an array of comments of a particular article with users that liked them
    
    [finishes #168516190]

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,11 +4,12 @@ import AuthMiddleware from '../middlewares/Authentication';
 import commentValidate from '../middlewares/commentValidations';
 
 const router = Router();
-const { likeComment, editComment } = CommentController;
+const { likeComment, editComment, getArticleComments } = CommentController;
 const { verifyToken, verifiedUserOnly } = AuthMiddleware;
 
 router.post('/likes/:commentId', verifyToken, verifiedUserOnly, likeComment);
 router.patch('/:commentId', verifyToken, verifiedUserOnly, commentValidate.comment, editComment);
+router.get('/:articleId', getArticleComments);
 
 
 export default router;
